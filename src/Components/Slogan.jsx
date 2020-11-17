512b8844b5559f7267ad0474cd159dc60f5cd5f6
Giang <kaylinpham109@gmail.com>
2020-11-17 20:47:24 +0700

    edit rules

--- src/Components/Slogan.jsx
+++ src/Components/Slogan.jsx
@@ -1,35 +1,44 @@
 import React, { Component } from "react";
 import "./css/Slogan.css";
-import firebase from "firebase/app";
+// import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
-import { firebaseConfig } from "../config/firebase.config";
-try {
-  firebase.initializeApp(firebaseConfig);
-} catch (e) {
-  console.log(e.message);
-}
+import firebase from '../config/firebase.config'
 const { default: Button } = require("./Button");
-const Slogan = () => {
-  const storage = firebase.storage();
-  var url = "";
-  storage.ref().child("dreaming____by_seerlight_dctbxag.jpg").getDownloadURL().then((downloadURL) =>{
-    console.log(downloadURL);
-  }).catch(err => console.log(err));
-  return (
-    <div className="slogan" style={{ backgroundImage: url }}>
-      <h1 className="title">Todo App</h1>
-      <p className="quote">
-        “Subtracting from your list of priorities is as important as adding to
-        it”
-      </p>
-      <div className="btns__container">
-        <Button>Login</Button>
-        <Button>Sign up</Button>
+class Slogan extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: null
+    }
+  }
+
+  componentDidMount() {
+    const storageRef = firebase.storage().ref();
+    let spaceRef = "dreaming____by_seerlight_dctbxag.jpg";
+    storageRef.child(spaceRef).getDownloadURL().then((url) => {
+      this.setState({url: url});
+    }).catch(err => console.log(err))
+    console.log(this.state.url)
+  }
+
+  render() {
+    return (
+      <div className="slogan" style={{backgroundImage: `url("${this.state.url}")`}}>
+        <h1 className="title">Todo App</h1>
+        <p className="quote">
+          “Subtracting from your list of priorities is as important as adding to
+          it”
+        </p>
+        <div className="btns__container">
+          <Button>Login</Button>
+          <Button>Sign up</Button>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
+  
 };
 
 export default Slogan;
--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,5 +1,7 @@
  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
- export const firebaseConfig = {
+ import firebase from 'firebase';
+
+  const firebaseConfig = {
   apiKey: "AIzaSyD27gAAerLYXkS8fnh8bssocBBeInj_ZeQ",
   authDomain: "dbp-cijs-d05-bd74a.firebaseapp.com",
   databaseURL: "https://dbp-cijs-d05-bd74a.firebaseio.com",
@@ -9,3 +11,7 @@
   appId: "1:832657027394:web:6533a28aedb4a9a5d9d8db",
   measurementId: "G-WJCNPTMPEB"
 };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
